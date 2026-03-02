060a10ac0f2679c98d1a2ece4dda011d8abd4a08
TheEpicStudent <3129289@smsd.org>
2026-03-02 15:38:43 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+const path = require('path');
+const prompt = require('prompt-sync')();
+const server = http.createServer(app);
+
+const { readFileSync } = require('fs');
+const PORT = process.env.PORT || 2000;
+
+app.use(express.static(path.join(__dirname + '/pages')));
+
+app.get('/', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    const filepath = path.join(__dirname, 'pages', 'index.html');
+    res.end(readFileSync(filepath));
+});
+
+
+
+server.listen(PORT, () => {
+    console.log(`Server is on http://localhost:${PORT}`);
+    console.log('-----------------------------------');
+});
